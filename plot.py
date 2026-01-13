b26efff1efd923876261ae3ca65ede2e75b78dd4
Ahad <mohdahad60080@gmail.com>
2026-01-13 22:10:33 +0530

    understood bare bones foundation

--- plot.py
+++ plot.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 x = [1, 2, 3, 4, 5]
 y = [2, 3, 5, 7, 11]
 
-plt.scatter(x, y)
+fig, ax = plt.subplots()
+ax.plot(x, y)
+ax.set_title("Explicit Control")
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
 plt.show()
-
+fig.savefig("plot.png")
