646f73f70fe1f8b228678fac522c1b17cc7ac896
Ahad <mohdahad60080@gmail.com>
2026-01-13 21:00:40 +0530

    scatter cos data is messy

--- plot.py
+++ plot.py
@@ -1,11 +1,8 @@
 import matplotlib.pyplot as plt
 
-x = [1, 2, 3, 4]
-y1 = [1, 4, 9, 16]
-y2 = [1, 2, 3, 4]
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
 
-plt.plot(x, y1, label="Squares")
-plt.plot(x, y2, label="Linear")
-
-plt.legend()
+plt.scatter(x, y)
 plt.show()
+
