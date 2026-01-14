e977000ea1c93025f7dc6a600c34951aae9caf77
Ahad <mohdahad60080@gmail.com>
2026-01-14 17:21:04 +0530

    better scattering

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
 
+x = [1,2,3,4,5]
+y = [1,4,9,16,25]
+
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.scatter(2,4, s=200)
+ax.scatter(x,y, s=200)
+
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 ax.tick_params(labelsize=14)
+
 plt.show()
