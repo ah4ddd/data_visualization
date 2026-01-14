f4576d25ce1fef1aca4ad5326a1ea1ebe7f35668
Ahad <mohdahad60080@gmail.com>
2026-01-14 18:53:57 +0530

    colorful saved amazing.. getting it

--- scatter_squares.py
+++ scatter_squares.py
@@ -5,13 +5,13 @@ y_val = [x**2 for x in x_val]
 
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.scatter(x_val, y_val, s=10)
+ax.scatter(x_val, y_val, c=y_val, cmap='Blues', s=10)
 
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 ax.tick_params(labelsize=14)
-
-ax.axis((0, 1100, 0, 1_100_000))
+ax.ticklabel_format(style='plain')
+plt.savefig('squares_plot.png', bbox_inches='tight')
 
 plt.show()
