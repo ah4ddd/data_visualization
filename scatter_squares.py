ce45c45adefb97a6c5966658d5be69c3b3db7901
Ahad <mohdahad60080@gmail.com>
2026-01-14 17:37:06 +0530

    upto 1000 plot points

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,15 +1,17 @@
 import matplotlib.pyplot as plt
 
-x = [1,2,3,4,5]
-y = [1,4,9,16,25]
+x_val = range(1, 1001)
+y_val = [x**2 for x in x_val]
 
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.scatter(x,y, s=200)
+ax.scatter(x_val, y_val, s=10)
 
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 ax.tick_params(labelsize=14)
+
+ax.axis((0, 1100, 0, 1_100_000))
 
 plt.show()
