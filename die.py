c57f11ec22dda2eb4be7bd489f5412c2b8544188
Ahad <mohdahad60080@gmail.com>
2026-01-15 17:27:41 +0530

    starting plotly

---
+++ die.py
@@ -0,0 +1,13 @@
+import plotly.express as px
+import pandas
+
+x = [1,2,3,4]
+y = [10,20,15,30]
+
+fig = px.line(
+    x=x,
+    y=y,
+    title="My First Plotly Chart",
+    labels={"x": "X Axis", "y": "Y Axis"}
+)
+fig.show()
