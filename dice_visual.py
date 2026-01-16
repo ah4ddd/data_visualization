c21e33262bf3f0ee4b60b9d062494a4902282773
Ahad <mohdahad60080@gmail.com>
2026-01-16 21:27:22 +0530

    copy of copy 2 die

---
+++ dice_visual.py
@@ -0,0 +1,26 @@
+from die import Die
+import plotly.express as px
+
+die_1 = Die()
+die_2 = Die()
+
+results = []
+for roll_num in range(1000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+frequencies = []
+max_results = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_results+1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
+
+fig = px.bar(x=poss_results,
+             y=frequencies,
+             title="Results of Rolling Two D6 1,000 Times",
+             labels={"x":"Results", "y":"Frequency of Results"}
+             )
+fig.show()
