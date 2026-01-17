72efaa1d6c7814d401976839bf1a6a10c4f8a169
Ahad <mohdahad60080@gmail.com>
2026-01-17 14:15:48 +0530

    extracting and reading data

--- downloading_data/sitka_high.py
+++ downloading_data/sitka_high.py
@@ -6,4 +6,13 @@ lines = path.read_text().splitlines()
 
 reader = csv.reader(lines)
 header_row = next(reader)
-print(header_row)
+
+for index, column_header in enumerate(header_row):
+    print(index, column_header)
+
+highs = []
+for row in reader:
+    high = int(row[4])
+    highs.append(high)
+
+print(highs)
