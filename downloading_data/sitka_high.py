8b66553073f248c3aaaf93aa87a7659412346152
Ahad <mohdahad60080@gmail.com>
2026-01-17 14:56:24 +0530

    shaded chart area on high and low

--- downloading_data/sitka_high.py
+++ downloading_data/sitka_high.py
@@ -21,8 +21,9 @@ for row in reader:
     highs.append(high)
     lows.append(low)
 
-ax.plot(dates, highs, color="red")
-ax.plot(dates, lows, color="blue")
+ax.plot(dates, highs, color="red", alpha=1)
+ax.plot(dates, lows, color="blue", alpha=1)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 ax.set_title("Daily High and Low Tempratures (Sitka, Alaska)", fontsize=24)
 ax.set_xlabel("", fontsize=16)
