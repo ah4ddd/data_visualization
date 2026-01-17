3cadae9e21a6ae4171789c34bbb875f9f3855ed1
Ahad <mohdahad60080@gmail.com>
2026-01-17 14:39:07 +0530

    plotted dates

--- downloading_data/sitka_high.py
+++ downloading_data/sitka_high.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import csv
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 plt.style.use('ggplot')
 fig, ax = plt.subplots()
@@ -11,20 +12,18 @@ lines = path.read_text().splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
 
-for index, column_header in enumerate(header_row):
-    print(index, column_header)
-
-highs = []
+dates, highs = [], []
 for row in reader:
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
     high = int(row[4])
+    dates.append(current_date)
     highs.append(high)
 
-print(highs)
-
-ax.plot(highs, color="red")
+ax.plot(dates, highs, color="red")
 
 ax.set_title("Daily High Tempratures, July 2021 (Sitka, Alaska)", fontsize=24)
 ax.set_xlabel("", fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel("Temprature (F)", fontsize=16)
 ax.tick_params(labelsize=16)
 
