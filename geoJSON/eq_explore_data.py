b6b49e5665355f9088110ecd43696de554fe9499
Ahad <mohdahad60080@gmail.com>
2026-01-18 16:51:39 +0530

    with latest lastweek data fk ya

--- geoJSON/eq_explore_data.py
+++ geoJSON/eq_explore_data.py
@@ -2,27 +2,26 @@ from pathlib import Path
 import json
 import plotly.express as px
 
-path = Path('eq_data/eq_data_30d_m1.geojson')
+path = Path('eq_data/latest_eq_past_week.geojson')
 contents = path.read_text()
 all_eq_data = json.loads(contents)
 
 all_eq_dicts = all_eq_data['features']
-print(len(all_eq_dicts))
 
-mags, lons, lats = [], [], []
+mags, lons, lats, eq_titles = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
+    if mag is None or mag <= 0:
+        continue
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    eq_title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
-
-print(mags[:10])
-print(lons[:5])
-print(lats[:5])
+    eq_titles.append(eq_title)
 
 title = 'Global Earthquakes'
-fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title, color=mags, color_continuous_scale='agsunset', labels={'color':'Magnitude'}, projection='natural earth')
+fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title, color=mags, color_continuous_scale='agsunset', labels={'color':'Magnitude'}, projection='natural earth', hover_name=eq_titles)
 
 fig.show()
