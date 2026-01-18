0f7f0647de582794cc2336ffe49e9a426a0f9c2c
Ahad <mohdahad60080@gmail.com>
2026-01-18 15:39:39 +0530

    extracting magnitudes

--- geoJSON/eq_explore_data.py
+++ geoJSON/eq_explore_data.py
@@ -8,3 +8,9 @@ all_eq_data = json.loads(contents)
 all_eq_dicts = all_eq_data['features']
 print(len(all_eq_dicts))
 
+mags = []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    mags.append(mag)
+
+print(mags[:10])
